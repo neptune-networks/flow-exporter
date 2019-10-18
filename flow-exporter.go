10bde13fe00c9aeade40780c26a73d95682ebbc4
Brooks Swinnerton <bswinnerton@gmail.com>
2019-10-17 20:40:26 -0400

    Add flags, log, and update metric names

--- flow-exporter.go
+++ flow-exporter.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"flag"
 	"github.com/Shopify/sarama"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
 )
+
+type config struct {
+	broker    string
+	topic     string
+	defaultAS int
+}
 
 type flow struct {
 	SourceAS      int    `json:"as_src"`
@@ -23,51 +29,65 @@ type flow struct {
 }
 
 var (
-	flowTrafficBytes = promauto.NewCounterVec(
+	flowReceiveBytesTotal = promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "flow_traffic_bytes",
+			Name: "flow_receive_bytes_total",
 			Help: "Bytes received.",
+		},
+		[]string{"source_as", "destination_as"},
+	)
+
+	flowTransmitBytesTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "flow_transmit_bytes_total",
+			Help: "Bytes transferred.",
 		},
 		[]string{"source_as", "destination_as"},
 	)
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <broker> <topic>\n", os.Args[0])
+	broker := flag.String("broker", "", "The Kafka broker to connect to")
+	topic := flag.String("topic", "", "The Kafka topic to consume from")
+	defaultAS := flag.Int("defaultAS", -1, "The autonomous system number to fall back to if not provided in flow")
+	flag.Parse()
+
+	options := config{broker: *broker, topic: *topic, defaultAS: *defaultAS}
+
+	if *broker == "" || *topic == "" {
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	broker := os.Args[1]
-	topic := os.Args[2]
-
 	go func() {
+		log.Info("Starting Prometheus web server")
 		http.Handle("/metrics", promhttp.Handler())
 		http.ListenAndServe(":9590", nil)
 	}()
-	createConsumer(broker, topic)
+	createConsumer(options)
 }
 
-func createConsumer(broker string, topic string) {
-	consumer, err := sarama.NewConsumer([]string{broker}, nil)
+func createConsumer(options config) {
+	log.Info("Starting Kafka consumer")
+	consumer, err := sarama.NewConsumer([]string{options.broker}, nil)
 	if err != nil {
 		panic(err)
 	}
 
 	defer func() {
 		if err := consumer.Close(); err != nil {
-			log.Fatalln(err)
+			log.Fatal(err)
 		}
 	}()
 
-	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	partitionConsumer, err := consumer.ConsumePartition(options.topic, 0, sarama.OffsetNewest)
 	if err != nil {
 		panic(err)
 	}
 
 	defer func() {
 		if err := partitionConsumer.Close(); err != nil {
-			log.Fatalln(err)
+			log.Fatal(err)
 		}
 	}()
 
@@ -81,14 +101,31 @@ ConsumerLoop:
 			var flow flow
 			json.Unmarshal([]byte(msg.Value), &flow)
 
-			flowTrafficBytes.With(
-				prometheus.Labels{
-					"source_as":      strconv.Itoa(flow.SourceAS),
-					"destination_as": strconv.Itoa(flow.DestinationAS),
-				},
-			).Add(float64(flow.Bytes))
+			if flow.SourceAS == 0 && flow.DestinationAS != 0 {
+				if options.defaultAS != -1 {
+					flow.SourceAS = options.defaultAS
+				}
 
-			log.Printf("Consumed message offset %d\n", msg.Offset)
+				flowTransmitBytesTotal.With(
+					prometheus.Labels{
+						"source_as":      strconv.Itoa(flow.SourceAS),
+						"destination_as": strconv.Itoa(flow.DestinationAS),
+					},
+				).Add(float64(flow.Bytes))
+			} else if flow.SourceAS != 0 && flow.DestinationAS == 0 {
+				if options.defaultAS != -1 {
+					flow.DestinationAS = options.defaultAS
+				}
+
+				flowReceiveBytesTotal.With(
+					prometheus.Labels{
+						"source_as":      strconv.Itoa(flow.SourceAS),
+						"destination_as": strconv.Itoa(flow.DestinationAS),
+					},
+				).Add(float64(flow.Bytes))
+			}
+
+			log.Debug("Consumed message offset %d\n", msg.Offset)
 		case <-signals:
 			break ConsumerLoop
 		}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/Shopify/sarama v1.24.0
 	github.com/prometheus/client_golang v1.2.1
+	github.com/sirupsen/logrus v1.4.2
 )
